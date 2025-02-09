e9a310f34bee1bf6980e7083644fc2f7f98c947b
GitKing-web <ekingagada007@gmail.com>
2025-02-09 15:12:30 -0600

    http create server removed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express'
-import http from 'http'
 import router from './routes/router.routes'
 const app = express()
-const server = http.createServer(app)
 
 
 app.use(express.json())
 app.use('/', router)
-export default server
+export default app
